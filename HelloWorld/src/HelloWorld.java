600636153669f07593de145ec4f09ccd12ed87c3
Vivi D. A <80104949+vividagustin@users.noreply.github.com>
2021-08-02 08:42:26 +0700

    add src to helloworld

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ *
+ * @author vivid
+ */
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        // (Edit so that it print a message other than Hello World)
+        System.out.println("Holla Java!");
+        
+        //(Error, semicolon important to Java syntax)
+        System.out.println("Hello World!")
+                
+        // (Error, there is package System not system. Java is case sensitive)
+        system.out.println("Hello World!");
+    }
+}
