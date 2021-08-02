1c5b42fefe69d61ed7b40fde759dd81f1162b058
Vivi D. A <80104949+vividagustin@users.noreply.github.com>
2021-08-02 08:45:24 +0700

    add src to problemset2_2

---
+++ ProblemSet2_2/src/problemset2_2/ProblemSet2_2.java
@@ -0,0 +1,70 @@
+/**
+ *
+ * @author vivid
+ */
+
+package problemset2_2;
+
+public class ProblemSet2_2 {
+    public static void main(String[] args) {
+        //Use print statements to create your own beautiful original ASCII art.
+        //Use comments to describe what your image is depicting.
+        
+        //ASCII art pohon
+        System.out.println("          **********            ");
+        System.out.println("     *********O******           ");
+        System.out.println("   **O***********O*******       ");
+        System.out.println("  ********O*********O*****      ");
+        System.out.println(" **O**********O*******O****     ");
+        System.out.println("*******O****O*******O*******    ");
+        System.out.println(" ***O**********O*********O***   ");
+        System.out.println("    ******O********O*******     ");
+        System.out.println("       ******************       ");
+        System.out.println("           |      |             ");
+        System.out.println("           |      |             ");
+        System.out.println("           |      |             ");
+        System.out.println("           |      |             ");
+        System.out.println("           |      |             ");
+        System.out.println("           |      |             ");
+        System.out.println("           |      |             ");
+        System.out.println("          /        \\           ");
+        System.out.println("        ///////\\\\\\\\\\\\\\   ");
+        System.out.println("      //////////\\\\\\\\\\\\\\\\");
+        
+        System.out.println(" " + "\n" + " " + "\n" + " ");
+        
+        //ASCII art bunga
+        System.out.println("         **     **             ");
+        System.out.println("        *  *   *  * **         ");
+        System.out.println("      **    ***   **  *        ");
+        System.out.println("      *      *    *   * **     ");
+        System.out.println("       *      *  *    **  **   ");
+        System.out.println("     ***    *******  *      *  ");
+        System.out.println("   **   *****......***  **** *  ");
+        System.out.println("   *      **........***    **  ");
+        System.out.println("   *     ****........**     ** ");
+        System.out.println("   *   **  ***......**       * ");
+        System.out.println("    * **     ***********     * ");
+        System.out.println("     **     **    **   ***  ** ");
+        System.out.println("     *     *       *     ****  ");
+        System.out.println("     *    *        *      **   ");
+        System.out.println("  *****  **        *      **** ");
+        System.out.println("  *    ****       ***    **  * ");
+        System.out.println("  * \\    ***   ******  ** / * ");
+        System.out.println("   *  \\     ***********  /  * ");
+        System.out.println("    **  \\     ** *|*    /  *  ");
+        System.out.println("     **   \\     **|*   /  *   ");
+        System.out.println("       **    \\   *|*  /  *    ");
+        System.out.println("        ****   **||* /  **     ");
+        System.out.println("            *** *|*   **       ");
+        System.out.println("               *|* ***         ");
+        System.out.println("               *|*             ");
+        System.out.println("                *|*            ");
+        System.out.println("                 *|*           ");
+        System.out.println("                  *|*          ");
+        System.out.println("                   *|*         ");
+        System.out.println("                    *|*        ");
+        System.out.println("                     *|*       ");
+        
+    } 
+}
