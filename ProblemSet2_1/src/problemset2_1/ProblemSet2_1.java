558cf70514a58ebd9ab2ab75d27880fad73acb8f
Vivi D. A <80104949+vividagustin@users.noreply.github.com>
2021-08-02 08:44:49 +0700

    add src to problemset2_1

---
+++ ProblemSet2_1/src/problemset2_1/ProblemSet2_1.java
@@ -0,0 +1,23 @@
+/**
+ *
+ * @author vivid
+ */
+
+package problemset2_1;
+
+public class ProblemSet2_1 {
+    public static void main(String[] args) {
+        //Use 8 print statements to print a smiley face.
+        //Your art will rely on only a single character, besides space, such as X or #.
+        
+        //use # to print a smiley face
+        System.out.println("  ####  ");
+        System.out.println(" #    # ");
+        System.out.println("# #  # #");
+        System.out.println("#      #");
+        System.out.println("# #  # #");
+        System.out.println("#  ##  #");
+        System.out.println(" #    # ");
+        System.out.println("  ####  ");
+    }   
+}
