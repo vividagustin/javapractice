60e8eb1c848e457b8086a111263bb03848da9170
Vivi D. A <80104949+vividagustin@users.noreply.github.com>
2021-08-02 08:39:58 +0700

    add src to text01

---
+++ JFo02_2_Exercise/Text01/src/text01/Text01.java
@@ -0,0 +1,39 @@
+/**
+ *
+ * @author vivid
+ */
+
+package text01;
+
+public class Text01 {
+    public static void main(String[] args) {
+        System.out.println("   /\\         /\\   "); System.out.println("  /  \\_______/  \\  "); System.out.println(" /               \\ ");
+        System.out.println("(  /\\         /\\  )");
+        System.out.println("====     V     ====");
+        System.out.println("======(__|__)======");
+        System.out.println("  (             )  ");
+        System.out.println("   (___________)   ");
+        
+        /*
+        1.Run the program normally: Breakpoints have no affect
+        2.Run the program with the debugger:
+            − Make sure the Output window is showing 
+            − Press Step Over to go to each next line
+        >>> The cat appear one line at a time
+        3.Modify the first 3 print statements appear on Line5
+        Run the program with the debugger:
+            − Make sure the Output window is showing
+            − Press Step Over Expression to go to each next line 
+            − Ignore the complicated code at the end of debugging
+        >>> The cat appear one line at a time, isn’t precise about whitespace
+        
+        Try removing a semicolon while debugging:
+        >>> The cat appear one line at a time, edit code has no affect
+        
+        Try removing a semicolon before debugging:
+        >>> error: ';' expected
+        The following error occurred while executing this line:
+        Compile failed; see the compiler error output for details.
+        */
+    }    
+}
