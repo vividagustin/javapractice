b51592e4afff9b273491953e812298f0caef45ec
Vivi D. A <80104949+vividagustin@users.noreply.github.com>
2021-08-11 16:43:57 +0700

    update src problem set 5_3 TrafficLightSwitch

--- ProblemSet5_3/TrafficLightSwitch/src/com/example/TrafficLightSwitch.java
+++ ProblemSet5_3/TrafficLightSwitch/src/com/example/TrafficLightSwitch.java
@@ -15,6 +15,7 @@ public class TrafficLightSwitch {
     public static void main(String args[]) {
         StopLight light = new StopLight();
         Scanner in = new Scanner(System.in);
+        String stop = "The Traffic Light is currently ";
         String next = "Next Traffic Light is ";
         String invalid = "Invalid color";
         
@@ -25,14 +26,17 @@ public class TrafficLightSwitch {
         //next color
         switch(currentColor){
             case 1:
+                System.out.println(stop + "red");
                 String nextLight1 = light.nextColor(currentColor);
                 System.out.println(next + nextLight1);
                 break;
             case 2:
+                System.out.println(stop + "green");
                 String nextLight2 = light.nextColor(currentColor);
                 System.out.println(next + nextLight2);
                 break;
             case 3:
+                System.out.println(stop + "yellow");
                 String nextLight3 = light.nextColor(currentColor);
                 System.out.println(next + nextLight3);
                 break;
